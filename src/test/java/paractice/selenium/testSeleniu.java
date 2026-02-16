90aa17b3b04c1b9a8ec7e3c0a73dab800436451f
Swagatika Mohapatra <swagatikamohapatra@Swagatikas-MacBook-Air.local>
2026-02-17 02:02:14 +0530

    latest code

--- src/test/java/paractice/selenium/testSeleniu.java
+++ src/test/java/paractice/selenium/testSeleniu.java
@@ -16,41 +16,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class testSeleniu {
 
-WebDriver driver ; 
+	WebDriver driver;
 
-@BeforeMethod
-@BeforeMethod()
+	@BeforeMethod
 
-public void setUp()
-{
-	WebDriverManager.chromiumdriver().setup();
-	driver = new ChromeDriver() ;
-	driver.get("https://learn-automation.com/implicit-wait-in-selenium-webdriver/");
-	String str =driver.findElement(By.xpath("//a[normalize-space()='Automation']")).getText();
-	System.out.println("The valueis "+ str);
-	
-	driver.manage().window().maximize();
-	
-	//h3[@class='t-title-s m-ellipsis-2lines][text()='Find The Perfect Hotel For Every Trip']
-	//*[@id="main-content"]/div/div[5]/div/div/div/div[1]/div/h3
-	
-	
-}
+	public void setUp() {
+		WebDriverManager.chromiumdriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://learn-automation.com/implicit-wait-in-selenium-webdriver/");
+		String str = driver.findElement(By.xpath("//a[normalize-space()='Automation']")).getText();
+		System.out.println("The valueis " + str);
 
-@Test()
+		driver.manage().window().maximize();
 
-public void googleTitle()
-{
-	driver.get("https://developer.chrome.com/docs/chromedriver/get-started/chromeos");
-}
+		// h3[@class='t-title-s m-ellipsis-2lines][text()='Find The Perfect Hotel For
+		// Every Trip']
+		// *[@id="main-content"]/div/div[5]/div/div/div/div[1]/div/h3
 
-@AfterMethod
-@AfterMethod
+	}
 
-public void tearDown()
-{
-	driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
-	driver.quit();
-}
+	@Test()
+
+	public void googleTitle() {
+		driver.get("https://developer.chrome.com/docs/chromedriver/get-started/chromeos");
+	}
+
+	@AfterMethod
+
+	public void tearDown() {
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+		driver.quit();
+	}
 
 }
